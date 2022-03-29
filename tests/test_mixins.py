99f9488bd0c977780844fd79743167b0010d359b
Cristian Garcia <cgarcia.e88@gmail.com>
2022-03-29 16:32:00 -0500

    fix apply

---
+++ tests/test_mixins.py
@@ -0,0 +1,40 @@
+import typing as tp
+from dataclasses import dataclass
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+import pytest
+
+import treeo as to
+
+
+class TestMixins:
+    def test_apply(self):
+        @dataclass
+        class SomeTree(to.Tree, to.Apply):
+            x: int = to.node()
+
+        tree = SomeTree(x=1)
+
+        def f(tree: SomeTree):
+            tree.x = 2
+
+        tree2 = tree.apply(f)
+
+        assert tree.x == 1
+        assert tree2.x == 2
+
+    def test_apply_inplace(self):
+        @dataclass
+        class SomeTree(to.Tree, to.Apply):
+            x: int = to.node()
+
+        tree = SomeTree(x=1)
+
+        def f(tree: SomeTree):
+            tree.x = 2
+
+        tree.apply(f, inplace=True)
+
+        assert tree.x == 2
